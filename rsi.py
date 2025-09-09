97642c347e7c2d5022465e8d91d9180529e663e5
vpg-per <venu.puram@gmail.com>
2025-09-08 20:02:53 -0400

    Add files via upload

--- rsi.py
+++ rsi.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-from flask import Flask
+import time
+from flask import Flask,json,render_template
 app = Flask(__name__)
 
 def fetch_stock_data(symbol="SPY", period="1y", interval="1d"):
@@ -44,13 +45,17 @@ def fetch_stock_data(symbol="SPY", period="1y", interval="1d"):
             'high': quotes['high'],
             'low': quotes['low'],
             'close': quotes['close'],
-            'volume': quotes['volume']
+            'volume': quotes['volume'],            
         })
         
         # Clean data (remove NaN values)
         df = df.dropna()
+        df['rec_dt']= df['timestamp'].dt.date
+        df['nmonth']= df['timestamp'].dt.strftime('%m')
+        df['nday']= df['timestamp'].dt.strftime('%d')
+        df['hour']= df['timestamp'].dt.strftime('%H')
+        df['minute']= df['timestamp'].dt.strftime('%M')
         df.set_index('timestamp', inplace=True)
-        
         return df
         
     except requests.exceptions.RequestException as e:
@@ -88,15 +93,12 @@ def calculate_rsi(df, period=14):
     df['rs'] = df['avg_gain'] / df['avg_loss']
     
     # Calculate RSI
-    df['rsi'] = 100 - (100 / (1 + df['rs']))
+    df['rsi'] = round(100 - (100 / (1 + df['rs'])),2)
     
     # Calculate Signal
-    df['signal'] = calculate_ema(df['rsi'], period=14)
+    df['signal'] = round(df['rsi'].ewm(span=period).mean(),2)
     
     return df
-
-def calculate_ema(data, period):
-    return data.ewm(span=period).mean()
 
 def identify_crossovers(df):
     """
@@ -112,52 +114,102 @@ def identify_crossovers(df):
     df['signal_prev'] = df['signal'].shift(1)
     
     # Bullish crossover: RSI crosses above Signal
-    df['bullish_crossover'] = (
+    bullflagval = (
         (df['rsi'] > df['signal']) & 
         (df['rsi_prev'] <= df['signal_prev'])
     )
+    df['bullish_crossover'] = bullflagval
     
     # Bearish crossover: RSI crosses below Signal
-    df['bearish_crossover'] = (
+    bearflagval = (
         (df['rsi'] < df['signal']) & 
         (df['rsi_prev'] >= df['signal_prev'])
     )
+    df['bearish_crossover'] = bearflagval
     
     return df
 
-@app.route("/")
-def home():
-    print("Fetching SPY data from Yahoo Finance...")
+def calculate_rsi_signal(df, interval="5m"):
+    df_signal = { }
+
+    if (interval=="5m"):
+        df_signal = df.copy()
+    elif (interval=="15m"):
+        df_signal = df[df['minute'].isin(["00", "15", "30", "45"])]
+    elif (interval=="30m"):
+        df_signal = df[df['minute'].isin(["00", "30"])]
+    elif (interval=="1h"):
+        df_signal = df[df['minute'].isin(["00"])]
+    elif (interval=="4h"):
+        df_signal = df[(df['hour'].isin(["00", "04", "08", "12", "16", "20", "24"]) & (df['minute'].isin(["00"])))]
     
-    # Fetch data
-    symbol="SPY"
-    df = fetch_stock_data(symbol, period="5d", interval="15m")
+    df_with_rsi = calculate_rsi(df_signal, period=14)    
+    df_final = identify_crossovers(df_with_rsi)
+    df_sel_cols = df_final.loc[:, ['rec_dt', 'nmonth', 'nday', 'hour', 'minute', 'close', 'rsi', 'signal', 'bullish_crossover', 'bearish_crossover']]
+    #df_filtered_rows = de_sel_cols = df_sel_cols[(df_sel_cols['bullish_crossover']==True) | (df_sel_cols['bearish_crossover']==True)]
+    
+    return df_sel_cols.tail(1)
+
+def calculate_stock_signal(symbol="SPY", period="1y", interval="1d"):
+    df = fetch_stock_data(symbol, period="5d", interval="5m")
     
     if df is None:
         print("Failed to fetch data. Please check your internet connection.")
         return
     
-    print(f"Successfully fetched {len(df)} days of data")
+    interval="5m"
+    alldatafs = {}
+    df_5m = calculate_rsi_signal(df, interval)
+    df_5m['interval'] = interval
+    df_alltfs = df_5m.copy()
     
-    # Calculate RSI
-    df_with_rsi = calculate_rsi(df, period=14)    
-    df_final = identify_crossovers(df_with_rsi)
-    df_reversed = df_final[::-1]
-                    
-    ret_val = "<table border='1' cellspacing='1' cellpadding='1'><tr><td>Time</td><td>" + symbol + " Symbol</td><td>Flag</td>"    
-    for date, row in df_reversed.iterrows():
-        flag_val = "" 
-        if (row['bullish_crossover']==True): 
-            flag_val = "Bullish"
-        elif (row['bearish_crossover']==True): 
-            flag_val = "Bearish"
-        if (flag_val != ""):
-            ret_val += "<tr><td>" + date.strftime('%m-%d %H:%M') + "</td><td>" + f"{row['close']:.2f}" + "</td><td>" + flag_val + "</td></tr>"
-    ret_val += "</table>"
+    interval="15m"
+    df_15m = calculate_rsi_signal(df)
+    df_15m['interval'] = interval
+    df_alltfs = pd.concat([df_alltfs, df_15m], ignore_index=False)
+
+    interval="30m"
+    df_30m = calculate_rsi_signal(df)
+    df_30m['interval'] = interval
+    df_alltfs = pd.concat([df_alltfs, df_30m], ignore_index=False)
+
+    interval="1h"
+    df_1h = calculate_rsi_signal(df)
+    df_1h['interval'] = interval
+    df_alltfs = pd.concat([df_alltfs, df_1h], ignore_index=False)
+
+    interval="4h"
+    df_4h = calculate_rsi_signal(df)
+    df_4h['interval']=interval
+    df_alltfs = pd.concat([df_alltfs, df_4h], ignore_index=False)
     
-    return ret_val
+    return df_alltfs
+
+@app.route('/returnPattern')
+def ReturnPattern():
+#def main():
+    stocksymbols = ['QQQ','IWM','GLD']
+    df_allsymbols = {}
+    for ss in stocksymbols:  
+        df_stock = calculate_stock_signal(ss, period="5d", interval="15m")
+        df_stock['symbol'] = ss
+        df_len = len(df_allsymbols)
+        if df_len == 0:
+            df_allsymbols = df_stock.copy()
+        else:
+            df_allsymbols = pd.concat([df_allsymbols, df_stock], ignore_index=False)
+    print(df_allsymbols)
+        #df_as_dict_list = df_stock.to_dict(orient='records')
+        #print(df_as_dict_list)
+        #df_allsymbols = pd.concat([df_allsymbols, {x: df_stock}], ignore_index=False)
+    
+    return df_allsymbols.to_json(orient='records', index=False)
+
+@app.route("/")
+def index():
+    return render_template('./index.html')
 
 if __name__ == "__main__":
     # Run the analysis
-    # spy_data = main()
+    #spy_data = main()
     app.run(host='0.0.0.0', port=80) 
